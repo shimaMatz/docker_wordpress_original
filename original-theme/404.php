a23fc472a3a58468ce37cd1b3e909e8fdd6e7849
shimamatz@gmail.com <shimamatz@gmail.com>
2021-09-23 19:42:25 +0900

    first commit

---
+++ original-theme/404.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @see https://codex.wordpress.org/Creating_an_Error_404_Page
+ */
+get_header();
+?>
+記事がありません。
+
+<?php
+get_footer();
